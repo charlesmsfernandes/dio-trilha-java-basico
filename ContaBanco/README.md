5d95656c80654a1ff7b3606a84692076760566e3
charlesmsfernandes <charlesmsfernandes@gmail.com>
2023-07-06 23:27:14 -0300

    Classe ContaTerminal Feita.

--- ContaBanco/README.md
+++ ContaBanco/README.md
@@ -1,21 +1,20 @@
 ## Getting Started
 
-A seguir, calcule a média do aluno, sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11). 
-Assuma que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
-
-Entrada
-O arquivo de entrada contém 2 valores com uma casa decimal cada um.
-
-Saída
-Calcule e imprima a variável MEDIA conforme exemplo abaixo, 
-com 5 dígitos após o ponto decimal e com um espaço em branco antes e depois da igualdade. 
-Utilize variáveis de dupla precisão (double) e como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
-
 
 TODO: Conhecer e importar a classe Scanner
 
-Exibir as ensgens para o nosso usuário
+Exibir as mensagens para o nosso usuário
+- receberá dados via terminal
+- contendo as características de conta em banco conforme atributos abaixo:
+Atributo	    Tipo	    Exemplo
+Numero	        Inteiro	    1021
+Agencia	        Texto	    067-8
+Nome Cliente    Texto	    MARIO ANDRADE
+Saldo	        Decimal	    237.48
 
 Obter pela scanner os valores digitados no terminal
+- Por favor, digite o número da Agência !
 
 Exibir a mensagem conta criada
+- Depois de todas as informações terem sido inseridas, o sistema deverá exibir a seguinte mensagem:
+"Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, sua agência é [Agencia], conta [Numero] e seu saldo [Saldo] já está disponível para saque".
--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Estou utilizando a estrutura try-with-resources
+        // para garantir que o Scanner seja fechado mesmo se uma exceção ocorrer
+        // e liberar os recursos associados a ele
+        try (Scanner scanner = new Scanner(System.in)) {
+            int numeroConta;
+            String agencia = ""; 
+            String nomeCliente = "";
+            double saldo; 
+
+            System.out.print("Por favor, digite o número da Conta: ");
+            numeroConta = scanner.nextInt();
+
+            System.out.print("Por favor, digite o número da Agência com - e digito (Ex: 067-8): ");
+            agencia = scanner.next();
+
+            // O scanner.next(), lê apenas a próxima sequência de caracteres até o próximo espaço em branco 
+            // ou quebra de linha. No entanto, ele não consome a quebra de linha. 
+            // Isso significa que, quando eu chamo o scanner.nextLine() imediatamente após scanner.next(), 
+            // ele encontra essa quebra de linha pendente e a consome, gerando um erro a aplicação.
+            // como scanner.nextLine() abaixo eu consumo a quebra de linha restante.
+            scanner.nextLine(); 
+
+            System.out.print("Por favor, digite o seu nome completo: ");
+            nomeCliente = scanner.nextLine();
+
+            System.out.print("Por favor, digite o seu Saldo atual (Ex: 237.48): ");
+            saldo = scanner.nextDouble();
+
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+        
+        }
     }
 }
